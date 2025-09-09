73cb7ade9699b8da8e8b7252baace10a4e8217fb
jdluiswise <jose_daniel@thewisedreams.com>
2025-09-09 14:02:12 +0200

    example proyect for clients with an integration on a jsx component with ts.

---
+++ src/App.tsx
@@ -0,0 +1,69 @@
+import "./App.css";
+import { useEffect } from "react";
+
+function App() {
+  useEffect(() => {
+    const handler = (event: { origin: string; data: { type: string; open: boolean; }; }) => {
+      if (event.origin !== "http://localhost:5174") return;
+      if (event.data.type === "resize") {
+        const iframe = document.getElementById("chatbot");
+        const buttonHeight = "68px";
+        const buttonWidth = "68px";
+        const modalHeiight = 600 + "px";
+        const modalWidth = 407 + "px";
+
+        if(!iframe) {
+          return;
+        }
+
+        if (event.data.open) {
+          iframe.style.height = modalHeiight;
+          iframe.style.width = modalWidth;
+        } else {
+          iframe.style.height = buttonHeight;
+          iframe.style.width = buttonWidth;
+        }
+        
+      }
+    };
+    window.addEventListener("message", handler);
+    return () => window.removeEventListener("message", handler);
+  }, []);
+  
+
+  return (
+    <>
+      <div>
+    
+        <h1 style={{color: "green"}}>
+          The Wise Dreams
+        </h1>
+        
+
+      </div>
+
+      <div style={{width: "clamp(0px, 95%, 600px)", textAlign:"center", margin: "auto"}}>
+        <p>This is an example of how one of our chatbots can be integrated into a client's website,
+           for instance, using an iframe in a modern React application</p>
+      </div>
+      <div className="card">
+        <p>
+          Example by
+          {" "}
+          <a href="thewisedreams.com" className="read-the-docs">
+            The Wise Dreams
+          </a>
+        </p>
+      </div>
+
+      <iframe
+        id="chatbot"
+        title="chatbot"
+        style={{ position: "absolute", right: 0, bottom: 0, border: "none" }}
+        src="http://localhost:5174/thewisedreams"
+      ></iframe>
+    </>
+  );
+}
+
+export default App;
